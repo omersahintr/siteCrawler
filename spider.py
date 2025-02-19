e372643a5be46ec0a85ad7c8840e0fecf3ec96eb
Omer Sahin <bilgi@omersahin.com.tr>
2025-02-19 19:09:16 +0300

    crawl function added

--- spider.py
+++ spider.py
@@ -1,8 +1,29 @@
 import requests as req
+from bs4 import BeautifulSoup
 
-siteUrl = input("Enter the site url: ")
+siteUrl = "https://omersahin.com.tr"
+foundLinks = []
 
-spider = req.get(siteUrl)
-print(spider.text)
+def make_req(url):
+    spider = req.get(url)
+    soup = BeautifulSoup(spider.text, "html.parser")
+    return soup
 
 # HTML Parse Process:
+
+def crawler(url):
+    countLink = 1
+    links = make_req(url)
+
+    for linkhref in links.find_all("a"): #<a href="https....">
+        foundLink = linkhref.get("href") # href="https...."
+
+        if foundLink:
+            if "#" in foundLink:
+                foundLink = foundLink.split("#")[0]
+            if foundLink not in foundLinks:
+                foundLinks.append(foundLink)
+                print(countLink, "-", foundLink)
+                crawler(foundLink) #recursive process.
+        countLink += 1
+crawler(siteUrl)
